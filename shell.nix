8d79a79f36d24854a8d7e6b3b7219f657dfc8ab9
samrose <samuel.rose@gmail.com>
2021-12-20 17:57:56 -0500

    streamlit with mach-nix

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import ./nixpkgs.nix {} }:
+with pkgs;
+let
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix";
+    ref = "refs/tags/3.3.0";
+  }) {
+    # optionally bring your own nixpkgs
+    # pkgs = import <nixpkgs> {};
+
+    # optionally specify the python version
+    python = "python39";
+
+    # optionally update pypi data revision from https://github.com/DavHau/pypi-deps-db
+    pypiDataRev = "65f1eac8364c73759b5f3dbddff4e2f61441c5b2";
+    pypiDataSha256 = "063bqj6i29285y2pyii9r8rn2sf5kddxa8lf83jllxhqd8pmis9i";
+  };
+in
+mach-nix.mkPythonShell {  # replace with mkPythonShell if shell is wanted
+  requirements = builtins.readFile ./requirements.txt;
+  _.gitpython.propagatedBuildInputs.mod = pySelf: self: oldVal: oldVal ++ [ pySelf.typing-extensions ];
+}
